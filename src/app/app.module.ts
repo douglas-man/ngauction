6bf927a68971e9db08b755cacdc276a1d89a45f4
douglas man <douglas.man.packt@outlook.com>
2020-07-08 23:12:53 -0400

    ng add @angular/localize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule,
     NgbModule,
+    BrowserModule,
     AppRoutingModule
   ],
   providers: [],
